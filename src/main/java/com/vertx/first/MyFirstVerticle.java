5e437a99970f42c8e50f44f87adbe6470dd8e4ef
William Powell <william.powell92@gmail.com>
2018-09-11 14:51:49 +0100

    Set up simple vert.x server

---
+++ src/main/java/com/vertx/first/MyFirstVerticle.java
@@ -0,0 +1,24 @@
+package com.vertx.first;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+
+public class MyFirstVerticle extends AbstractVerticle {
+
+  @Override
+  public void start(Future<Void> future) {
+    vertx
+        .createHttpServer()
+        .requestHandler(r -> {
+          r.response().end("<h1>Look at me, I'm Mr Meeseeks!</h1>");
+        })
+        .listen(8080, result -> {
+          if (result.succeeded()) {
+            future.complete();
+          } else {
+            future.fail(result.cause());
+          }
+        });
+  }
+
+}
